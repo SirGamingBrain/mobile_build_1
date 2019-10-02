9120d7bfb799c37285b5c80868d065a1796cfa0b
SirGamingBrain <chastan72@gmail.com>
2019-10-01 21:50:15 -0400

    Updated Scene ready for models and needs work

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Camera main;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = this.GetComponent<Camera>();
+        player = GameObject.Find("Test Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Rotate();
+        this.transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerTestMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition(transform.position + (transform.forward * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.MovePosition(transform.position + (-transform.right * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition(transform.position + (-transform.forward * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(transform.position + (transform.right * Time.deltaTime));
+        }
+    }
+}
