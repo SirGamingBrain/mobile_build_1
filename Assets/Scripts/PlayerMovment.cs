6b15b219353b92066dc7c6e5d6e993bf4c2333b3
SirGamingBrain <chastan72@gmail.com>
2019-10-07 00:27:31 -0400

    Got a basic ninja
    Ninja time.

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -6,6 +6,8 @@ public class PlayerMovment : MonoBehaviour
 {
     Rigidbody rb;
 
+    public Animator ninja;
+
     protected Joystick joystick;
 
     bool moving = false;
@@ -13,81 +15,90 @@ public class PlayerMovment : MonoBehaviour
     float xSpeed = 0f;
     float zSpeed = 0f;
 
+    float heading = 0f;
+
+    float debugTimer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
 
         joystick = FindObjectOfType<Joystick>();
+
+        ninja = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log((joystick.Horizontal * .5f) + " and " + (joystick.Vertical * .5f));
+        //A timer to debug all of the things that I need to track.
+        debugTimer += Time.deltaTime;
 
+        if (debugTimer >= 1f)
+        {
+            Debug.Log("Moving: " + moving);
+            Debug.Log("Horizontal Speed: " + xSpeed);
+            Debug.Log("Vertical Speed: " + zSpeed);
+            debugTimer = 0f;
+        }
 
-        //this.transform.LookAt(new Vector3(0f,0f,0f));
-
-        //Update the speed of the player.
-
+        //Checking to see if the player is moving, updating some animations.
         if (joystick.Horizontal == 0 || joystick.Vertical == 0)
         {
             moving = false;
+            ninja.SetBool("Running", false);
         }
         else
         {
             moving = true;
+            ninja.SetBool("Running", true);
         }
 
-        if (moving == true) {
-            float tempXSpeed = 1f * (joystick.Horizontal * .5f);
-
-            if (xSpeed < 2.5f && xSpeed > -2.5f)
-            {
-                xSpeed += tempXSpeed;
-            }
-
-            float tempZSpeed = 1f * (joystick.Vertical * .5f);
-            zSpeed += tempZSpeed;
-
-            if (zSpeed < 2.5f && zSpeed > -2.5f)
-            {
-                zSpeed += tempZSpeed;
-            }
-        }
-
-        if (moving == false)
+        //Updating the player's speed and direction of movement if they are moving, otherwise slow them overtime.
+        if (moving == true)
         {
-            if (xSpeed < .1f && xSpeed > -.1f)
-            {
-                xSpeed = 0f;
-            }
-            else if (xSpeed < 0f)
-            {
-                xSpeed += .1f;
-            }
-            else if (xSpeed > 0f)
+            xSpeed = joystick.Horizontal * 5f;
+            zSpeed = joystick.Vertical * 5f;
+        }
+        else
+        {
+            if (xSpeed > 0.1f)
             {
                 xSpeed -= .1f;
             }
+            else if (xSpeed < -0.1f)
+            {
+                xSpeed += .1f;
+            }
+            else
+            {
+                xSpeed = 0f;
+            }
 
-            if (zSpeed < .1f && zSpeed > -.1f)
-            {
-                zSpeed = 0f;
-            }
-            else if (zSpeed < 0f)
-            {
-                zSpeed += .1f;
-            }
-            else if (zSpeed > 0f)
+            if (zSpeed > 0f)
             {
                 zSpeed -= .1f;
             }
+            else if (zSpeed < -0.1f)
+            {
+                zSpeed += .1f;
+            }
+            else
+            {
+                zSpeed = 0f;
+            }
         }
 
-        Debug.Log(xSpeed + "and speeds" + zSpeed);
+        rb.velocity = new Vector3(xSpeed, 0f,zSpeed);
 
-        rb.velocity = new Vector3(xSpeed, 0,zSpeed);
+        //Updating the player's direction if they are moving.
+        if (moving) {
+            heading = Mathf.Atan2(joystick.Horizontal, joystick.Vertical);
+            this.transform.rotation = Quaternion.Euler(0f, heading * Mathf.Rad2Deg, 0f);
+        }
+
+        //Updating the state of animation of the player based on what is happening.
+
     }
 }
