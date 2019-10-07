7a671f9405f56dca9d718a638653d0daa2cafbb3
OriginalNappyBoi <54566412+OriginalNappyBoi@users.noreply.github.com>
2019-10-06 23:21:22 -0400

    My first push

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] Enemies = new GameObject[4];
+
+    int waveCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void WaveManagement()
+    {
+
+    }
+}
