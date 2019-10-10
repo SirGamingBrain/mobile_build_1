d0fec7c8805f4d86f2df75af3d68e7e3854c1740
OriginalNappyBoi <pnelso16@students.kennesaw.edu>
2019-10-10 01:14:16 -0400

    debugging
    nothing big yet hopefully either tonight or tomorrow I will be pushing a big update with attacking AI and correct wave and spawn management

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -6,12 +6,13 @@ public class Enemies : MonoBehaviour
 {
    
     float enemyHP = 100;
+    GameObject GameController;
 
     bool isAlive = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameController = GameObject.FindGameObjectWithTag("GameController");
     }
 
     // Update is called once per frame
@@ -20,19 +21,14 @@ public class Enemies : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            enemyHP = 0;
-            isAlive = false;
             onDeath();
         }
-        onDeath();
+        
     }
 
     void onDeath()
     {
-        if (!isAlive)
-        {
-            gameObject.SendMessage("EnemyNumber", SendMessageOptions.DontRequireReceiver);
-            Destroy(this.gameObject);
-        }
+        Destroy(this.gameObject);
+        GameController.SendMessage("EnemiesNumber", SendMessageOptions.DontRequireReceiver);
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+    //use compare tag to check if there are any enemies in no enemies increase the wave count and start spawning
     public GameObject[] Enemies = new GameObject[4];
     public Transform[] spawnpoint = new Transform[3];
 
@@ -16,32 +18,32 @@ public class GameController : MonoBehaviour
     void Start()
     {
         waveCounter = 1;
+        isSpawned = true;
         WaveManagement();
-    }
-
-    private void Awake()
-    {
         
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         EnemiesGone();
-       
+        Debug.Log(Enemycounter);
+
         //check to see if all the enmies have been killed and then increase the coutner
     }
 
     void WaveManagement()
     {
-        isSpawned = true;
-        if (waveCounter == 1)
+       
+        if (waveCounter == 1 && isSpawned)
         {
             for(int i = 0; i<spawnpoint.Length-1; i++)
             {
                 Instantiate(Enemies[0], spawnpoint[i].position, spawnpoint[i].rotation);
                 //spawn an enemy
-                Debug.Log(spawnpoint[i]);
+                Debug.Log("this the spawnpoints"+ " " + spawnpoint[i]);
                 if (spawnpoint[i] == spawnpoint[1])
                 {
                     i++;
@@ -51,19 +53,17 @@ public class GameController : MonoBehaviour
 
                 }
             }
+
            
-
-
-            isSpawned = false;
         }
 
-        if (waveCounter == 2)
+        if (waveCounter == 2 && isSpawned)
         {
             for (int i = 0; i < spawnpoint.Length - 1; i++)
             {
                 Instantiate(Enemies[1], spawnpoint[i].position, spawnpoint[i].rotation);
                 //spawn an enemy
-                Debug.Log(spawnpoint[i]);
+                Debug.Log("this the spawnpoints" + " " + spawnpoint[i]);
                 if (spawnpoint[i] == spawnpoint[1])
                 {
                     i++;
@@ -73,16 +73,16 @@ public class GameController : MonoBehaviour
 
                 }
             }
-            isSpawned = false;
+           
         }
 
-        if (waveCounter == 3)
+        if (waveCounter == 3 && isSpawned)
         {
             for (int i = 0; i < spawnpoint.Length - 1; i++)
             {
                 Instantiate(Enemies[2], spawnpoint[i].position, spawnpoint[i].rotation);
                 //spawn an enemy
-                Debug.Log(spawnpoint[i]);
+                Debug.Log("this the spawnpoints" + " " + spawnpoint[i]);
                 if (spawnpoint[i] == spawnpoint[1])
                 {
                     i++;
@@ -92,16 +92,16 @@ public class GameController : MonoBehaviour
 
                 }
             }
-            isSpawned = false;
+           
         }
 
-        if (waveCounter == 4)
+        if (waveCounter == 4 && isSpawned)
         {
             for (int i = 0; i < spawnpoint.Length - 1; i++)
             {
                 Instantiate(Enemies[3], spawnpoint[i].position, spawnpoint[i].rotation);
                 //spawn an enemy
-                Debug.Log(spawnpoint[i]);
+                Debug.Log("this the spawnpoints" + " " + spawnpoint[i]);
                 if (spawnpoint[i] == spawnpoint[1])
                 {
                     i++;
@@ -111,32 +111,41 @@ public class GameController : MonoBehaviour
 
                 }
             }
-            isSpawned = false;
 
+            isSpawned = false;
         }
 
-
+       
     }
 
     void EnemiesGone()
     {
-      
-       
         //all the enemies are dead then increment wave counter
         if (Enemycounter == 3)
         {
             //all the enemies are killed you can now increment the waves manager
             waveCounter++;
-            Enemycounter = 0;
+            isSpawned = true;
+            StartCoroutine(waveSpawning());
             Debug.Log("This is the next Wave:" + "" + waveCounter);
-            WaveManagement();
+            Enemycounter = 0;
+
         }
     }
 
     void EnemyNumber()
     {
-        Enemycounter++;
-        Debug.Log(Enemycounter);
+        Enemycounter = 3;
+        Debug.Log("this the enemies:" + " " + Enemycounter);
+        
 
+    }
+
+    IEnumerator waveSpawning()
+    {
+        yield return new WaitForSeconds(3);
+        WaveManagement();
+        Debug.Log("Am i here?");
+        isSpawned = false;
     }
 }
