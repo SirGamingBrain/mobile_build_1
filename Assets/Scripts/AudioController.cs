eedefd3c629b75f749a7b7055813478a47b25902
SirGamingBrain <chastan72@gmail.com>
2019-10-12 01:52:36 -0400

    Finished Main Menu
    Basic Main Menu Complete with These:
     - Added background music.
     - Added GUI Button sounds.
     - Added a settings page to change the volume and quality of the game itself.
     - Added a start game, continue game, and quit game button.
    - Small decorations and flower petals to add to the scene.

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public float masterVolume = 100f;
+
+    float timer = 0f;
+
+    public AudioSource backgroundMusic;
+
+    AudioSource[] sources;
+
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.SetFloat("Volume", 100);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        masterVolume = PlayerPrefs.GetFloat("Volume");
+
+        sources = FindObjectsOfType<AudioSource>();
+
+        foreach (AudioSource source in sources)
+        {
+            if (source.name == "Background Music")
+            {
+                source.volume = masterVolume/100;
+                Debug.Log("We have identified the background music!");
+            }
+            else
+            {
+                source.volume = masterVolume / 100;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 3f)
+        {
+            Debug.Log("Master Volume: " + masterVolume);
+            timer = 0f;
+        }
+
+        if (PlayerPrefs.GetFloat("Volume") != masterVolume)
+        {
+            PlayerPrefs.SetFloat("Volume", masterVolume);
+            UpdateAudioSources();
+        }
+
+        
+    }
+
+    public void UpdateAudioSources()
+    {
+        foreach (AudioSource source in sources)
+        {
+            if (source.name == "Background Music")
+            {
+                source.volume = masterVolume/100;
+            }
+            else
+            {
+                source.volume = masterVolume/100;
+            }
+        }
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,18 +1,138 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioController audioScript;
+
+    public AudioSource touchSounds;
+
+    public Slider settingsVolume;
+
+    public GameObject settings;
+
+    public TextMeshProUGUI volumeText;
+    public TextMeshProUGUI qualityText;
+
+    string[] names;
+
+    string currentQuality;
+
+    int qualityIndex;
+
+    public void Awake()
     {
-        
+        names = QualitySettings.names;
+
+        if (!PlayerPrefs.HasKey("Quality Level"))
+        {
+            currentQuality = "Medium";
+            PlayerPrefs.SetString("Quality Level", "Medium");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    public void Start()
     {
-        
+        settingsVolume.value = PlayerPrefs.GetFloat("Volume");
+        settings = GameObject.Find("Settings Page");
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (PlayerPrefs.GetString("Quality Level") == names[i])
+            {
+                QualitySettings.SetQualityLevel(i, true);
+                qualityText.text = names[i];
+                qualityIndex = i;
+            }
+        }
+
+        settings.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        //Fly and Tilt the Camera Up, then load the scene and fly back down.
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void ExitGame()
+    {
+        //Save the current settings, and close the application.
+        Application.Quit();
+    }
+
+    public void ContinueGame()
+    {
+        //Load the last level you were on.
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Settings()
+    {
+        //Open the settings page to edit volume and quality.
+        //Or close it, depends on if the page is open.
+        if (settings.activeSelf)
+        {
+            PlayerPrefs.Save();
+            settings.SetActive(false);
+        }
+        else
+        {
+            settings.SetActive(true);
+        }
+    }
+
+    public void SaveandExit()
+    {
+        Settings();
+        PlayerPrefs.Save();
+    }
+
+    public void Volume(Slider volume)
+    {
+        audioScript.masterVolume = volume.value;
+        volumeText.text = ("" + volume.value);
+    }
+
+    public void OnTouch()
+    {
+        touchSounds.Play();
+    }
+
+    public void IncreaseQuality()
+    {
+        if (qualityIndex < 5)
+        {
+            qualityIndex++;
+            QualitySettings.SetQualityLevel(qualityIndex, true);
+            qualityText.text = names[qualityIndex];
+        }
+        else
+        {
+            qualityIndex = 0;
+            QualitySettings.SetQualityLevel(qualityIndex, true);
+            qualityText.text = names[qualityIndex];
+        }
+    }
+
+    public void DecreaseQuality()
+    {
+        if (qualityIndex > 0)
+        {
+            qualityIndex--;
+            QualitySettings.SetQualityLevel(qualityIndex, true);
+            qualityText.text = names[qualityIndex];
+        }
+        else
+        {
+            qualityIndex = 5;
+            QualitySettings.SetQualityLevel(qualityIndex, true);
+            qualityText.text = names[qualityIndex];
+        }
     }
 }
